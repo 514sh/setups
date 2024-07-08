de89318abb8cf7c8a1b60fc32845c0f0bd0b5a1c
Mark Balagtas <mark.joseph.balagtas@outlook.com>
2024-07-08 15:45:29 +0800

    added: install script - lens-desktop

--- scripts/installations/install-all.sh
+++ scripts/installations/install-all.sh
@@ -8,6 +8,7 @@ cd /home/$1/setup/scripts/installations
 ./install-docker.sh
 ./install-post-docker.sh
 ./install-kubectl.sh
+./install-lens-desktop.sh
 
 wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 #./install-spotify.sh
